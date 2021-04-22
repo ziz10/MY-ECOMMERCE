08a06ffc0653dd14f376e2e1ceea3228760dabcc
Abdoul Bass <malickya8891@gmail.com>
2021-04-21 21:29:10 -0700

    set up fields and rules from product model

--- Homework/Develop/models/Product.js
+++ Homework/Develop/models/Product.js
@@ -8,16 +8,52 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define product_name column
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define price column
+        price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        // define stock column
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+        // define category_id column
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "category",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+    }
 );
 
 module.exports = Product;
