cfad139a36e410b3db7940359dc56f7324117ace
Abdoul Bass <malickya8891@gmail.com>
2021-04-21 22:31:09 -0700

    created psot method to create a product

--- Homework/Develop/routes/api/category-routes.js
+++ Homework/Develop/routes/api/category-routes.js
@@ -51,15 +51,17 @@ router.get('/:id', (req, res) => {
 });
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  router.post('/', (req, res) => {
+    // create a new category
+    Category.create({
+        category_name: req.body.category_name
+    })
+        .then(dbCategoryData => res.json(dbCategoryData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
-
-router.post('/', (req, res) => {
-  // create a new category
-});
-
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
