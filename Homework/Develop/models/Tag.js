40d38865bff225b936cc664bc241ed4181f99335
Abdoul Bass <malickya8891@gmail.com>
2021-04-21 21:33:13 -0700

    initialized tag file with properties

--- Homework/Develop/models/Tag.js
+++ Homework/Develop/models/Tag.js
@@ -5,16 +5,26 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define tag_name column
+        tag_name: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
 );
 
 module.exports = Tag;
