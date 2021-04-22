45ae2ef651b76715c5aa33da0ca253fbea0c4ecc
Abdoul Bass <malickya8891@gmail.com>
2021-04-21 21:13:23 -0700

    initialized productTag.js file

--- Homework/Develop/models/ProductTag.js
+++ Homework/Develop/models/ProductTag.js
@@ -1,3 +1,4 @@
+  
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
@@ -5,30 +6,38 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-  },
-  // define category_name column
-  category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-  }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define product_id column
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "product",
+                key: "id"
+            }
+        },
+        // define tag_id column
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "tag",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    }
 );
 
 module.exports = ProductTag;
-
-
-
